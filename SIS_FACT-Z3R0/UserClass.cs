c9689695eaacd8471bdae3092fd733d57f9929c0
Jean Carlos Reyes Encarnacion <yacalo45@hotmail.es>
2021-06-02 23:50:15 -0400

    USER CLASS WITH LOGIN

---
+++ SIS_FACT-Z3R0/UserClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS_FACT_Z3R0
+{
+    public class UserClass
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
